45689b61b1d7e00a072d8170d67f284a9a91f450
AdriansRepos <ferencik.adrian@outlook.cz>
2026-04-25 19:40:32 +0200

    Oprava a zjednodušení metody pro výpočty

--- VyukaNasobilky/Vypocty.cs
+++ VyukaNasobilky/Vypocty.cs
@@ -1,39 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace VyukaNasobilky
 {
-    class Vypocty(int prvniCislo, int druheCislo)
+    class Vypocty
     {
-        public int PrvniCislo { get; } = prvniCislo;
-        public int DruheCislo { get; } = druheCislo;
+        public int PrvniCislo { get; }
+        public int DruheCislo { get; }
         public int Vysledek => PrvniCislo * DruheCislo;
+
+        public Vypocty(int prvniCislo, int druheCislo)
+        {
+            PrvniCislo = prvniCislo;
+            DruheCislo = druheCislo;
+        }
 
         public override string ToString()
         {
             return $"{PrvniCislo} × {DruheCislo} = ";
-        }               
+        }    
 
-        public void KontrolaOdpovedi(int odpoved)
-        {
-            if (odpoved == Vysledek)
-            {
-                Console.WriteLine("Správně!");
-            }
-            else
-            {
-                Console.WriteLine($"Špatně! Správná odpověď je {Vysledek}.");
-            }
-        }
-        /*public void GenerujPriklad()
-        {
-            Random random = new Random();
-            PrvniCislo = random.Next(1, 10);
-            DruheCislo = random.Next(1, 10);
-            Vysledek = PrvniCislo * DruheCislo;
-        }*/
+    /*public void GenerujPriklad()
+    {
+        Random random = new Random();
+        PrvniCislo = random.Next(1, 10);
+        DruheCislo = random.Next(1, 10);
+        Vysledek = PrvniCislo * DruheCislo;
+    }*/
     }
 }
